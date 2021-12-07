e9d908347d83902031260ef7bc8f8928b0534a77
= <=>
2021-12-07 10:22:43 +0700

    update

--- src/component.js
+++ src/component.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+import Header from "./components/header";
+
 function Component() {
   return (
     //   Code Here
-    <div></div>
+    <div>
+      <Header />
+      <Content />
+    </div>
   );
 }
 
 // Create a new component here
+function Content() {
+  return (
+    <div>
+      <h1>This is content</h1>
+      <button>Click me</button>
+    </div>
+  );
+}
 
 export default Component;
--- src/components/header.js
+++ src/components/header.js
@@ -1,1 +1,11 @@
 // Code Here
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <h1>This Is Header</h1>
+        </div>
+    );
+}
+export default Header;
