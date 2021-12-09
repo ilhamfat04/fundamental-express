5d952709cc911131b0cc814e00b82a304ce16f1a
= <=>
2021-12-09 11:41:10 +0700

    update

--- src/state.js
+++ src/state.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function State() {
   //init State
+  const [counter, setCounter] = useState(0)
 
   //Create Function Add
+  function Add() {
+
+  }
 
   //Create Function Less
 
