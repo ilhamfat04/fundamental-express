9f876a9cbd3fc5efb83855593f830b960a5995b5
= <=>
2021-12-09 11:25:43 +0700

    update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,25 @@
 // code Here
+
+// penulisan 3
+// function List({ listData, color, qty }) {
+function List(props) {
+    // console.log(props);
+
+    // penulisan 2
+    const { listData, color, qty } = props
+
+    return (
+        <div>
+            {/* penulisan 1 */}
+            {/* <h1>{props.listData} - {props.color} - {props.qty} </h1> */}
+
+            {/* penulisan 2 */}
+            <h1>{listData} - {color} - {qty} </h1>
+
+
+        </div>
+    )
+
+}
+
+export default List
--- src/props.js
+++ src/props.js
@@ -1,11 +1,24 @@
 import React from "react";
 
 //Import Components
+import List from "./components/list";
 
 function Props() {
+
+  // tambahan
+  const firstCarName = "BMW"
+  const firstCarColor = "Red"
+  const firstCarQty = 234
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>On the image element using default props, namely src</p>
+      <img src="https://images.unsplash.com/photo-1638825794622-4c3bb400cdef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+
+      <List listData={firstCarName} color={firstCarColor} qty={firstCarQty} />
+      <List listData="Mercedes Benz" />
+      <List listData="Buggati" />
+    </div>
   );
 }
 
